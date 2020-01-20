1d0abb869c80a4cdb593b1950118f66b3add03e4
Thomas <thomas.alberts@student.hu.nl>
2020-01-20 15:09:14 +0100

    poging tot java

--- src/main/java/domain/Generate.java
+++ src/main/java/domain/Generate.java
@@ -5,5 +5,18 @@ import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 import domain.*;
+public class jood(){
+        "CREATE OR REPLACE TRIGGER {name}\n" +
+        "BEFORE INSERT OR UPDATE ON {table}\n" +
+        "WHEN {column} {operator} {start_range} AND {end_range}\n" +
+        "DECLARE \n" +
+        "  rule xcep EXCEPTION \n" +
+        "BEGIN \n" +
+        "  rule xcep EXCEPTION \n" +
+        "EXCEPTION\n" +
+        "  WHEN RULE XCEP THEN\n" +
+        "    DBMS_OUTPUT.PUT_LINE('DIT MAG NIET! REGEL OVERTREDEN NIFFO')\n" +
+        "END {name}";
+private String
 
-//to do
+        }
