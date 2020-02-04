369f7f2d086bb8b47b1c14ca7e8a2530ecf3e437
Hvitson <Thomas_alberts@live.nl>
2020-02-04 11:56:31 +0100

    poging tot java part 1231123

---
+++ src/main/java/persistence/BusinessRuleOracleDaoImpl.java
@@ -0,0 +1,70 @@
+package persistence;
+
+import persistence.BusinessRuleOracleDao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import domain.BusinessRule;
+
+public class BusinessRuleOracleDaoImpl extends OracleBaseDao implements BusinessRuleOracleDao {
+
+    public BusinessRuleOracleDaoImpl() throws SQLException {
+        super();
+    }
+
+    public BusinessRule save(BusinessRule b) {
+        try {
+            PreparedStatement pstmnt = OracleBaseDao.conn.prepareStatement("INSERT INTO businessrule (naam, type, omschrijving, foutmelding, status) VALUES (?, ?, ?, ?, ?)");
+            pstmnt.setString(1, b.getNaam());
+            pstmnt.setString(2, b.getType());
+            pstmnt.setString(3, b.getOmschrijving());
+            pstmnt.setString(4, b.getFoutmelding());
+            pstmnt.setInt(5, b.getStatus());
+
+            pstmnt.executeUpdate();
+
+            pstmnt.close();
+        } catch (SQLException sqlex) {
+            System.out.println("SQLException: " + sqlex.getMessage());
+        }
+
+        return b;
+    }
+
+    public BusinessRule update(BusinessRule b) {
+        try {
+            PreparedStatement pstmnt = OracleBaseDao.conn.prepareStatement("UPDATE businessrule SET naam = ?, type = ?, omschrijving = ?, foutmelding = ?, status = ? WHERE naam = ?");
+            pstmnt.setString(1, b.getNaam());
+            pstmnt.setString(2, b.getType());
+            pstmnt.setString(3, b.getOmschrijving());
+            pstmnt.setString(4, b.getFoutmelding());
+            pstmnt.setInt(5, b.getStatus());
+
+            pstmnt.executeUpdate();
+
+            pstmnt.close();
+        }
+        catch (SQLException sqlex) {
+            System.out.println("SQLException: " + sqlex.getMessage());
+        }
+
+        return b;
+    }
+
+    public boolean delete(BusinessRule b) {
+        try {
+            PreparedStatement pstmnt = OracleBaseDao.conn.prepareStatement("DELETE FROM businessrule WHERE naam = ?");
+            pstmnt.setString(1, b.getNaam());
+            pstmnt.executeUpdate();
+
+
+            return true;
+        } catch (SQLException sqlex) {
+            System.out.println("SQLException: " + sqlex.getMessage());
+        }
+
+        return false;
+    }
+}
+
